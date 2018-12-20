2acba4e768cc881596bfd1d4b3def531926125ce
Galilea Lopez <galilealopez@MBP-de-Galilea.localdomain>
2018-12-20 17:34:28 -0600

    Only managers and admins can delete tickets

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -31,6 +31,10 @@ RSpec.feature 'Users can only see the appropiate links' do
       visit project_ticket_path(project, ticket)
       expect(page).not_to have_link 'Edit Ticket'
     end
+    scenario 'cannot see the Delete Ticket  link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link 'Delete Ticket'
+    end
   end
 
   context 'regular users' do
@@ -62,6 +66,10 @@ RSpec.feature 'Users can only see the appropiate links' do
     scenario 'can see the Edit Ticket link' do
       visit project_ticket_path(project, ticket)
       expect(page).to have_link 'Edit Ticket'
+    end
+    scenario 'can see the Delete Ticket  link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link 'Delete Ticket'
     end
   end
 end
--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -11,30 +11,36 @@ RSpec.describe TicketPolicy do
       let(:user) { nil }
       it { should_not permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
     context 'for viewers of the  project' do
       before { assign_role!(user, :viewer,  project) }
       it { should permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
     context 'for editors of the project' do
       before { assign_role!(user, :editor, project) }
       it { should permit_action :show }
       it { should permit_action :create }
       it { should permit_action :update }
+      it { should_not permit_action :destroy }
     end
     context 'for managers of other projects' do
       before { assign_role!(:user, :manager, FactoryBot.create(:project)) }
-      it { should_not permit_action :show }
+      it { should  permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should  permit_action :destroy }
     end
     context 'for dministrators' do
       let(:user) { FactoryBot.create :user, :admin }
       it { should permit_action :show }
       it { should permit_action :create }
       it { should permit_action :update }
+      it { should permit_action :destroy }
     end
   end
 end
